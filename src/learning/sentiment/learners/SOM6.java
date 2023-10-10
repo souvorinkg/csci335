7d158521d1e3b74b298d20fb1f6508e84d555a41
gjf2a <ferrer@hendrix.edu>
2023-10-10 07:43:17 -0500

    Updated NaiveBayes.java.

--- src/learning/sentiment/learners/SOM6.java
+++
@@ -1,9 +0,0 @@
-package learning.sentiment.learners;
-
-import learning.sentiment.core.SOMSentimentBridge;
-
-public class SOM6 extends SOMSentimentBridge {
-    public SOM6() {
-        super(6);
-    }
-}
