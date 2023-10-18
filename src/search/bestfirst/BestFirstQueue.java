7a847e5a8d53cd1639c89989ecdffd010780ecef
gjf2a <ferrer@hendrix.edu>
2023-10-18 14:13:27 -0500

    Updated BestFirstQueue to  make it easier to understand and implement.

--- src/search/bestfirst/BestFirstQueue.java
+++ src/search/bestfirst/BestFirstQueue.java
@@ -1,27 +1,40 @@
 package search.bestfirst;
 
+import core.Duple;
 import search.SearchNode;
 import search.SearchQueue;
 
+import java.util.HashMap;
 import java.util.Optional;
+import java.util.PriorityQueue;
 import java.util.function.ToIntFunction;
 
 public class BestFirstQueue<T> implements SearchQueue<T> {
-    // TODO: Implement this class
-    // HINT: Use java.util.PriorityQueue. It will really help you.
+    // Each object in the priority queue is an estimate paired with a SearchNode.
+    private PriorityQueue<Duple<Integer,SearchNode<T>>> queue;
+
+    // For each object encountered, this is the lowest total length estimate
+    // encountered so far.
+    private HashMap<T,Integer> lowestEstimateFor;
+
+    // Use this heuristic to get the estimated distance to the goal node.
+    private ToIntFunction<T> heuristic;
 
     public BestFirstQueue(ToIntFunction<T> heuristic) {
-        // TODO: Your code here
+        // TODO: Initialize the three objects listed above.
     }
 
     @Override
     public void enqueue(SearchNode<T> node) {
-        // TODO: Your code here
+        // TODO: Calculate the total distance estimate. Then
+        //  add the node to the queue if there is no current estimate for the node
+        //  or if the new estimate is lower than the lowest seen so far.
     }
 
     @Override
     public Optional<SearchNode<T>> dequeue() {
-        // TODO: Your code here
+        // TODO: If the queue is empty, return Optional.empty(), otherwise, return
+        //  whichever object is next from the priority queue.
         return Optional.empty(); // TODO: Replace this line; it is here to let the code compile.
     }
 }
